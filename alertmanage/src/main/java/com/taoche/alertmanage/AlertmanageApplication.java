a363fb86fceae568e740cfe8c5a6d46f38b69340
liudengfeng <liudengfeng@taoche.com>
2021-11-04 09:27:53 +0800

    alertmanager

---
+++ alertmanage/src/main/java/com/taoche/alertmanage/AlertmanageApplication.java
@@ -0,0 +1,13 @@
+package com.taoche.alertmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlertmanageApplication.class, args);
+    }
+
+}
---
+++ alertmanage/src/test/java/com/taoche/alertmanage/AlertmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoche.alertmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
