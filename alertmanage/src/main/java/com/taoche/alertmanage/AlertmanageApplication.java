9f4313580eefa3616a05c06b910118dadb774c97
liudengfeng <liudengfeng@taoche.com>
2021-11-04 09:58:12 +0800

    启动提交

--- alertmanage/src/main/java/com/taoche/alertmanage/AlertmanageApplication.java
+++ alertmanage/src/main/java/com/taoche/alertmanage/AlertmanageApplication.java
@@ -1,13 +1,18 @@
 package com.taoche.alertmanage;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlertmanageApplication {
+public class AlertmanageApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AlertmanageApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("application started!");
+    }
 }
