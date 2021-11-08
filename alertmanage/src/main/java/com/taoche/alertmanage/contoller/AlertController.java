5e355b7852254dbdf5d6785c3fe73229b527a245
liudengfeng <liudengfeng@taoche.com>
2021-11-08 17:45:03 +0800

    加入controller层的处理

---
+++ alertmanage/src/main/java/com/taoche/alertmanage/contoller/AlertController.java
@@ -0,0 +1,50 @@
+package com.taoche.alertmanage.contoller;
+
+import com.taoche.alertmanage.constants.EAlertProcessType;
+import com.taoche.alertmanage.dto.ResultDto;
+import com.taoche.alertmanage.service.impl.AlertManageProcessContext;
+import com.taoche.alertmanage.service.impl.GenerateResultFactory;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+
+@RestController
+@RequestMapping("/alert")
+public class AlertController {
+    @Autowired
+    private AlertManageProcessContext alertManageProcessContext;
+
+    /**
+     * ip访问频率预警
+     *
+     * @param ip
+     * @return
+     */
+    @GetMapping("/ip")
+    public ResultDto alertIp(@RequestParam String ip) {
+        if (StringUtils.isBlank(ip)) {
+            return GenerateResultFactory.generateFailureResultOfMsg("访问ip不能为空!", null);
+        }
+        ResultDto resultDto = this.alertManageProcessContext.alertProcess(EAlertProcessType.IP, ip);
+
+        return resultDto;
+    }
+
+    /**
+     * 手机号访问频率预警
+     *
+     * @param mobile
+     * @return
+     */
+    @GetMapping("/mobile")
+    public ResultDto alertMobile(@RequestParam String mobile) {
+        if (StringUtils.isBlank(mobile)) {
+            return GenerateResultFactory.generateFailureResultOfMsg("手机号不能为空!", null);
+        }
+        ResultDto resultDto = this.alertManageProcessContext.alertProcess(EAlertProcessType.MOBILE, mobile);
+
+        return resultDto;
+    }
+}
